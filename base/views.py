973cf6b1bcd88a0474c4487f412c8fe863054bd8
Adarsh <adarshriit2017@gmail.com>
2023-06-24 17:20:12 +0530

    24062023-database & Auth

--- base/views.py
+++ base/views.py
@@ -1,25 +1,24 @@
 from django.shortcuts import render
+from .models import Room
+
 
 # Create your views here.
 
-rooms = [
-    {'id':1,'name':'Lets learn Python'},
-    {'id':2,'name':'frontend developers'},
-    {'id':3,'name':'design with me'},
-
-]
+# rooms = [
+#     {'id':1,'name':'Lets learn Python'},
+#     {'id':2,'name':'frontend developers'},
+#     {'id':3,'name':'design with me'},
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request,'base/home.html',context)
     # return render(request,'home.html',{'rooms':rooms})
 
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id = pk)
     
     context = {'room':room}
 
